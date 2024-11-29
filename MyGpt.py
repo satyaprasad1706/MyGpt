0e83ed5570c2b76ecbb9f1c98395dff5cd2b9f0c
Vakkalagadda Hima Venkata Satya Prasad <satyaprasad1706@gmail.com>
2024-11-29 13:06:43 +0530

    Add files via upload

---
+++ MyGpt.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import requests
+
+
+
+st.title("MyGpt")
+
+if "message" not in st.session_state:
+    st.session_state.message = []
+
+for message in st.session_state.message:
+    with st.chat_message(message['role']):
+        st.write(message['content'])
+
+
+prompt = st.chat_input("ask anything...")
+with st.chat_message("user"):
+    st.write(prompt)
+st.session_state.message.append({'role': 'user','content':prompt})
+
+if prompt:
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyAy0hYLok7rvd436GHGCfcu58Sn_XR_-M8"
+
+    data = {
+        'contents':[{'parts':[{'text':prompt}]}]
+    
+    }
+    response = requests.post(url,json=data)
+    response = (response.json())
+    response = response['candidates'][0]['content']['parts'][0]['text']
+
+try:
+    
+    with st.chat_message("assistant"):
+        st.write(response)
+    st.session_state.message.append({'role': 'assistant','content':response})
+except:
+        pass
